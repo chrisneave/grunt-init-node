1c263eee131c6509c20c3fb38ae8b1e5c0a15fe2
Chris Neave <chrispneave@gmail.com>
2013-12-21 21:24:16 +0000

    Grunt tasks for mocha and istanbul code coverage

--- root/Gruntfile.js
+++ root/Gruntfile.js
@@ -28,21 +28,54 @@ module.exports = function(grunt) {
       },
       lib: {
         files: '<%= jshint.lib.src %>',
-        tasks: ['jshint:lib', 'nodeunit']
+        tasks: ['jshint:lib', 'simplemocha']
       },
       test: {
         files: '<%= jshint.test.src %>',
-        tasks: ['jshint:test', 'nodeunit']
+        tasks: ['jshint:test', 'simplemocha']
       },
     },
+    simplemocha: {
+      all: {
+        src: ['test/**/*.js'],
+        options: {
+          globals: ['chai'],
+          timeout: 2000,
+          ignoreLeaks: false,
+          reporter: 'dot'
+        }
+      }
+    },
+    coverage: {
+      options: {
+        thresholds: {
+          statements: 90,
+          branches: 90,
+          lines: 90,
+          functions: 90
+        },
+        dir: 'build/coverage'
+      }
+    },
+    clean: ['coverage'],
+    open: {
+      cover: {
+        path: 'build/coverage/lcov-report/index.html',
+        app: 'Google Chrome'
+      }
+    }
   });
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-simple-mocha');
+  grunt.loadNpmTasks('grunt-istanbul-coverage');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-open');
 
   // Default task.
-  grunt.registerTask('default', ['jshint', 'nodeunit']);
-
+  grunt.registerTask('default', ['jshint', 'simplemocha']);
+  grunt.registerTask('cover', ['clear', 'clean', 'istanbul', 'open:cover']);
 };
